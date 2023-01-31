c23ec2f9b97d04e8e347dbb08f9284ef00fe7585
Md Hasan <mdhasan8064@gmail.com>
2023-01-31 14:34:29 +0600

    Basic layout & tailwind

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+
+
+const About = () => {
+    return (
+        <div className="text-green-300 text-center mt-10  font-bold text-3xl">
+            <h1>Hello Next js. I am In</h1>
+        </div>
+    );
+};
+
+export default About;
